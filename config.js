6b3117cee44fdd84ecb81d4d482b29c62d3908db
Amy Yoder <amyj_510@hotmail.com>
2021-11-26 07:51:01 -0500

    OAuth and User Authentication

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '335443271726118',
+        clientSecret: '9d69154d147690b486e40dea98b8e887'
+    }
 }
